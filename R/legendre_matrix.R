cc3e1a762f0ca23338d2bab5e4e579bd24f59dd5
masuday <masdee@gmail.com>
2023-07-14 17:21:26 +0900

    added arguments, x.min and x.max

--- R/legendre_matrix.R
+++ R/legendre_matrix.R
@@ -1,10 +1,20 @@
 # library(mpoly)
 
-legendre.matrix <- function(x, n, dig=NULL){
+# Arguments
+#  x : original variable
+#  n : degree of Legendre polynomials
+#  x.min : lower bound of x (default = min(x))
+#  x.max : upper bound of x (default = max(x))
+#  dif : significant digits for output (default = no rounding)
+
+legendre.matrix <- function(x, n, x.min=NULL, x.max=NULL, dig=NULL){
    legf <- as.function( legendre(0:n,normalized=TRUE) )
-   x.min <- min(x)
-   x.max <- max(x)
-   y <- -1 + 2*( (x - x.min)/(x.max-x.min) )
+   if(is.null(x.min)){ xmin <- min(x) }
+   else              { xmin <- x.min }
+   if(is.null(x.max)){ xmax<- max(x) }
+   else              { xmax <- x.max }
+   #y <- -1 + 2*( (x - xmin)/(xmax-xmin) )
+   y <- -1 + 2*( (x - min(xmin,xmax))/(max(xmin,xmax)-min(xmin,xmax)) )
    M <- legf(y)
    if(!is.null(dig)){
       M <- signif(legf(y),dig)
