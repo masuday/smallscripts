309cdcb83cd34c2317756ca1741d190bf0964896
masuday <masdee@gmail.com>
2024-03-08 11:20:13 +0900

    add effect assignment example

---
+++ R/stat_sim/lm_effect_assignment.R
@@ -0,0 +1,44 @@
+#
+# effect assignment
+#
+
+# -------------------------------------------
+# regression
+# x: independent variable
+# n: sample size
+n <- 10
+
+# uniform distribution [1,5]
+x <- round(runif(n,1,5))
+
+# another method (better)
+x <- sample(1:5, n, TRUE)
+
+# b0, b1: pre-defined regression coefficients
+b0 <- 1.5
+b1 <- 2.0
+m <- 
+
+# e: random error (normal distribution)
+e <- rnorm(n, mean=0, sd=10)
+
+# observation
+y <- m + e
+
+# -------------------------------------------
+# class effect
+n <- 10
+nlev <- 3
+eff <- rnorm(nlev,mean=0,sd=10)
+
+# periodic assignment 1
+a <- as.factor(rep(1:nlev,length.out=n))
+
+# periodic assignment 2
+a <- as.factor(sort(rep(1:nlev,length.out=n)))
+
+# random assignment
+a <- as.factor(sample(1:nlev, n, TRUE))
+
+# effect size
+m <- eff[a]
